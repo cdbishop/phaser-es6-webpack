c8c6fe83ef26e630199b12011f39782a3f12f8db
Craig Bishop <craig.bishop@vcatechnology.com>
2018-05-30 12:15:01 +0100

    cleanup

--- src/sprites/Mushroom.js
+++ src/sprites/Mushroom.js
@@ -4,9 +4,15 @@ export default class extends Phaser.Sprite {
   constructor ({ game, x, y, asset }) {
     super(game, x, y, asset)
     this.anchor.setTo(0.5)
+    this.movement = 10;
   }
 
   update () {
     this.angle += 1
+    this.x += this.movement
+    if (this.x >= this.game.width)
+      this.movement *= -1;
+    else if (this.x <= 0)
+      this.movement *= -1;
   }
 }
